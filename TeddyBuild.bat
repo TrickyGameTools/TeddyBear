7d8aa97377c8b74a875d7cab807e692a776a0e23
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-15 20:11:37 +0100

    Builder update

--- TeddyBuild.bat
+++ TeddyBuild.bat
@@ -5,15 +5,16 @@
    goto BUILDLAUNCHER
    
 :DEFINEMSBUILD
-   set TRICKYMSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
+   echo:Configuring
+   set TRICKYMSBUILD=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe
    
 :BUILDLAUNCHER
-   echo 
+   echo Compiling Launcher
    cd TeddyLaunch
-   %TRICKYMSBUILD% /p:Configuration=Release TeddyBear.sln > ..\ErrorLog\Launch.txt
+   "%TRICKYMSBUILD%" /p:Configuration=Release TeddyBear.sln > ..\ErrorLog\Launch.txt
    if errorlevel 1 goto ERRORLAUNCHER
    cd ..
-   goto BUILDEDITOR
+   goto BUILDWIZARD
    
    
 :ERRORLAUNCHER
@@ -23,12 +24,37 @@
    echo:
    type ErrorLog\Launch.txt
    goto :FINALE
+
+:BUILDWIZARD
+   echo Compiling Wizard
+   cd TeddyWizard 
+   "%TRICKYMSBUILD%" /p:Configuration=Release TeddyWizard.sln > ..\ErrorLog\Wizard.txt
+   if errorlevel 1 goto ERRORWizard
+   cd ..                                                                        
+   goto BUILDEDITOR
+
+:ERRORWIZARD                                                                    
+   cd ..                                                                        
+   cls                                                                          
+   echo:Building the wizard failed!                                             
+   echo:                                                                        
+   type ErrorLog\Wizard.txt
+   goto :FINALE
    
 :BUILDEDITOR
+   echo Compiling Editor
    cd TeddyEdit
    %TRICKYMSBUILD% /p:Configuration=Release TeddyEdit.sln > ..\ErrorLog\Edit.txt
    if errorlevel 1 goto ERROREDITOR
    cd ..
+   goto DORELEASE
+
+
+:DORELEASE
+   echo:Creating Release
+   copy TeddyLaunch\bin\release\*.exe Release
+   copy TeddyWizard\bin\release\*.exe Release
+   copy TeddyEditor\bin\release\*.exe Release
    goto FINALE
    
    
@@ -43,4 +69,5 @@
 :FINALE
    echo:
    echo:Have a nice day!
+   
    
