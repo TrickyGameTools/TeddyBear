acff1e93a0e0e9a6e3551606a51eb54c8ec8c2d8
Jeroen Broks <facebook@tbbs.nl>
2019-03-15 13:28:49 +0100

    Create TeddyCore.cs

---
+++ TeddyClass/TeddyCore.cs
@@ -0,0 +1,12 @@
+using TrickyUnits;
+using UseJCR6;
+
+namespace TeddyBear{
+
+    class Core{
+        static void Init(){
+            MKL_Version("","");
+            MKL_Lic("","");
+        }
+    }
+}
