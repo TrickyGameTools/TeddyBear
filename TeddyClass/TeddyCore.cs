bd640581c24c3f39c5ca652f17677d5162c12c48
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-16 10:12:18 +0100

    Basic defs

--- TeddyClass/TeddyCore.cs
+++ TeddyClass/TeddyCore.cs
@@ -1,7 +1,7 @@
 // Lic:
 // TeddyClass/TeddyCore.cs
 // TeddyBear Core
-// version: 19.03.15
+// version: 19.03.16
 // Copyright (C)  Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
@@ -18,15 +18,51 @@
 // 3. This notice may not be removed or altered from any source distribution.
 // EndLic
 
+
+
 using TrickyUnits;
+using System;
+using System.Collections.Generic;
 using UseJCR6;
 
 namespace TeddyBear{
 
+    class TeddyLayer {
+
+        public byte[,] buf;
+
+
+
+        public TeddyLayer(int width,int height) {
+
+            if (width < 2 || height < 2) throw new Exception($"Format of layer must be at least 2x2. {width}x{height} is therefore not valid!");
+
+            buf = new byte[height, width];
+
+        }
+
+
+
+        public byte Get(int x, int y) => buf[y, x];
+
+        public void Put(int x, int y, byte v) { buf[y, x] = v; }
+
+    }
+
+    class TeddyMap {
+
+        SortedDictionary<string, TeddyLayer> Layers = new SortedDictionary<string, TeddyLayer>();
+
+
+
+        public void NewLayer(string name,int w,int h) { Layers[name] = new TeddyLayer(w, h); }
+
+    }
+
     class Core{
         static void Init(){
-            MKL_Version("","");
-            MKL_Lic("","");
+            MKL.Version("TeddyBear - TeddyCore.cs","19.03.16");
+            MKL.Lic    ("TeddyBear - TeddyCore.cs","ZLib License");
         }
     }
 }
