a957b6592b40e87813768cf2f63b62742de74d10
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-22 10:28:04 +0100

    Basis Texture Load

---
+++ TeddyEdit/Stages/TextureLoad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace TeddyEdit.Stages
+{
+    class TextureLoad : BasisStage
+    {
+        static TextureLoad me = new TextureLoad();
+
+        public override void Draw(Game1 game, GameTime gameTime)
+        {
+            
+        }
+
+        public override void Update(Game1 game, GameTime gameTime, MouseState mouse, GamePadState gamepad, KeyboardState kb)
+        {
+            
+        }
+
+        public static void ComeToMe()
+        {
+            ProjectData.Game.SetStage(me);
+        }
+
+    }
+}
