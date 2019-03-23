6b1f0b46508e514f65998c2c8991e03ad75cd03d
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-23 15:30:25 +0100

    Less is more

--- log.ps1
+++ log.ps1
@@ -1,3 +1,3 @@
 # All this file does is showing the debug log (if there actually is one)
-type "E:/Home/Temp/TeddyLog" | more
-# Is there a Windows variant of "less"?
+type "E:/Home/Temp/TeddyLog" | less
+# Less is more
