bebce4be6f6b7fdb01548b099798c040761b47a3
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-28 16:58:06 +0100

    Basis Draw (no content yet)

---
+++ TeddyClass/TeddyDraw.cs
@@ -0,0 +1,7 @@
+namespace TeddyBear{
+
+/// <summary>This class contains the basic draw routines. Please note, as I wanted TeddyBear to be as Flexible as possible, it's driver based. So you'll need to attach a driver to this class in order to make it work!</summary>
+class TeddyDraw {
+}
+
+}
