c71bf6b42c3a1a43e0a13f923dacf88b3558873a
Jeroen P. Broks <facebook@tbbs.nl>
2019-03-30 22:38:11 +0100

    Ctrl-Q will not save the map and quit teddybear.

--- TeddyEdit/Game1.cs
+++ TeddyEdit/Game1.cs
@@ -157,8 +157,10 @@ namespace TeddyEdit
             var gpstate = GamePad.GetState(PlayerIndex.One);
             var kbstate = Keyboard.GetState();
             // This line will for now remain, but may have to leave once the editor is really getting to move 
-            if (gpstate.Buttons.Back == ButtonState.Pressed || kbstate.IsKeyDown(Keys.Escape))
+            if (kbstate.IsKeyDown(Keys.Q) && (kbstate.IsKeyDown(Keys.LeftControl) || kbstate.IsKeyDown(Keys.RightControl))) {
+                Main.Me.SaveMap();
                 Exit();
+            }
 
             // TODO: Add your update logic here
             var mstate = Mouse.GetState();
